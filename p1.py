230f99fd4305baff39490ffcf99224dc3f7579ca
blam <notblam@gmail.com>
2020-09-17 20:28:28 -0600

    added second neuron layer

--- p1.py
+++ p1.py
@@ -10,5 +10,13 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, -0.73, 0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
